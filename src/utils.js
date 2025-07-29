1f51eb6054d0c69cea7b3317420870f9d23e27bf
Aditya79l <adityaungati@gmail.com>
2025-07-29 19:18:15 +0530

    Set API URL to Render backend for production

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 // utils.js
-const apiURL =
-  "http://localhost:7000" || "https://chat-app-backend-n2mj.onrender.com";
+const apiURL = "https://chat-app-backend-n2mj.onrender.com";
+
 export default apiURL;
